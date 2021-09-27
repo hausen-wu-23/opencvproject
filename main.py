5c258d90c84d3eea5682eb175b22f79f8ea184ae
Hausen <70286690+hausen-wu-23@users.noreply.github.com>
2021-09-26 22:49:05 -0700

    rewrote code in functions for readability

--- main.py
+++ main.py
@@ -4,61 +4,70 @@ import argparse
 import cv2
 import imutils
 
-# adding argument and getting the image
-parse = argparse.ArgumentParser()
-parse.add_argument('-i', '--image', required=True, help='path to image')
-args = vars(parse.parse_args())
+def findDoc(original, edged):
+    # find contours in the image
+    # using RETR_LIST to find all contours
+    (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-# reading the original image
-original = cv2.imread(args['image'])
+    # sort the contours from the largest to smallest
+    # only taking the 10 largest one 
+    cnts = sorted(cnts, key = cv2.contourArea, reverse=True)
 
-# resizing the image down for more accurate detection and faster processing
-original = imutils.resize(original, width=600)
+    # copying image for contour drawing
+    cont = original.copy()
 
-# creating a copy for modification
-img = original.copy()
+    # loop through all countours
+    for c in cnts:
+        # approximate the contour length
+        p = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.05 * p, True)
 
-# convert image to grayscale
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # if 4 points counted, we can copy the contour 
+        if len(approx) == 4:
+            doc_cnt = approx
+            break
 
-# relatively high amount of blurring to avoid text on the document to 
-# mess up the edge detection process
-# blurred = cv2.bilateralFilter(img, 11, 17, 17)
-blurred = cv2.GaussianBlur(img, (9, 9), 0)
-# blurred = cv2.medianBlur(img, 9)
+    return (cont, doc_cnt)
 
-# display original and processed image
-cv2.imshow('original', original)
-cv2.imshow('processed for edge detection', blurred)
+def main():
+    # adding argument and getting the image
+    parse = argparse.ArgumentParser()
+    parse.add_argument('-i', '--image', required=True, help='path to image')
+    args = vars(parse.parse_args())
 
-# canny edge detection
-edged = cv2.Canny(blurred, 80, 250)
-cv2.imshow("edges", edged)
+    # reading the original image
+    original = cv2.imread(args['image'])
 
-# find contours in the image
-# using RETR_LIST to find all contours
-(cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # resizing the image down for more accurate detection and faster processing
+    original = imutils.resize(original, width=600)
 
-# sort the contours from the largest to smallest
-# only taking the 10 largest one 
-cnts = sorted(cnts, key = cv2.contourArea, reverse=True)
+    # creating a copy for modification
+    img = original.copy()
 
-# copying image for contour drawing
-cont = original.copy()
+    # convert image to grayscale
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# loop through all countours
-for c in cnts:
-    # approximate the contour length
-    p = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.05 * p, True)
+    # relatively high amount of blurring to avoid text on the document to 
+    # mess up the edge detection process
+    # blurred = cv2.bilateralFilter(img, 11, 17, 17)
+    blurred = cv2.GaussianBlur(img, (9, 9), 0)
+    # blurred = cv2.medianBlur(img, 9)
 
-    # if 4 points counted, we can copy the contour 
-    if len(approx) == 4:
-        doc_cnt = approx
-        break
+    # display original and processed image
+    cv2.imshow('original', original)
+    cv2.imshow('processed for edge detection', blurred)
 
-# draw contours around the file
-cv2.drawContours(cont, [doc_cnt], -1, (0, 0, 255), 5)
-cv2.imshow('contours', cont)
+    # canny edge detection
+    edged = cv2.Canny(blurred, 80, 250)
+    cv2.imshow("edges", edged)
 
-cv2.waitKey(0)
+    (cont, doc_cnt) = findDoc(original, edged)
+
+    # draw contours around the file
+    cv2.drawContours(cont, [doc_cnt], -1, (0, 0, 255), 5)
+    cv2.imshow('contours', cont)
+
+    cv2.waitKey(0)
+
+if __name__ == '__main__':
+    main()
